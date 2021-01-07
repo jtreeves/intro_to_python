08e579ea6114e7afb4c8bd026b84049a761cf6c5
jtreeves <jackson.t.reeves@gmail.com>
2021-01-07 10:59:21 -0500

    [examples] Outline basics of Python

---
+++ examples.py
@@ -0,0 +1,37 @@
+# comment
+
+"""
+Multiline
+Comment
+"""
+
+name = 'Jackson'
+age = 34
+is_old = False
+sei_1019 = True
+
+print(name)
+
+name = 'Lev'
+
+print(name)
+
+# Methods for a String
+
+sentence = "Today is Nicole's birthday"
+nicole_birthday = sentence.split(' ')
+print(nicole_birthday)
+
+new_sentence = (' ').join(nicole_birthday)
+# OR: new_sentence = (' ').join(nicole_birthday)
+
+print(new_sentence)
+
+# Find index
+print(new_sentence.index('N'))
+
+# Upper case everything
+name.upper()
+
+# Lower case everything
+name.lower()
